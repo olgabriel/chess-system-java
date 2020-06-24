97d5202ccf3833190b4c7ebcda92f7010504e696
Gabriel <gabrielustosa@gmail.com>
2020-06-24 15:58:12 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public static void main(String[] args) {
 			if (capturedPiece != null) {
 				captured.add(capturedPiece);
 			}
+			
+			if (chessMatch.getPromoted() != null) {
+				System.out.print("Enter piece for promotion (B/N/R/Q): ");
+				String type = sc.nextLine();
+				chessMatch.replacePromotedPiece(type);
+			}
 		}
 		catch (ChessException e) {
 			System.out.println(e.getMessage());
